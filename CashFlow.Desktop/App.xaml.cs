4c6dc4e2096fdb6000f1a11a497276a5d7cf600b
elitonluiz1989 <elitonluiz1989@gmail.com>
2021-11-28 21:10:17 -0400

    Add desktop project

---
+++ CashFlow.Desktop/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using CashFlow.Desktop.Tools;
+using CashFlow.Desktop.Views;
+using CashFlow.Infra.Data.Context;
+using CashFlow.Infra.Data.Mapping;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Windows;
+
+namespace CashFlow.Desktop
+{
+    public partial class App : Application
+    {
+        private readonly IServiceProvider _serviceProvider;
+        public App()
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddSingleton<SQLiteContext>();
+
+            services.AddSingleton<MainWindow>();
+
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+        public async void AppStartup(object sender, StartupEventArgs e)
+        {
+            SQLiteContext dbContext = _serviceProvider.GetRequiredService<SQLiteContext>();
+            await dbContext.InitializeDatabase();
+
+            ServiceProviderAcessor.Initialize(_serviceProvider);
+            AppMapper.Initialize();
+
+            MainWindow mainWindow = _serviceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+        }
+    }
+}
---
+++ CashFlow.Desktop/Views/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace CashFlow.Desktop.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
